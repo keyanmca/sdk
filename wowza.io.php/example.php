ea692e5a86765392a6013e58a05404c72efe452e
Carlos Camacho <carlos.camacho@frontiersin.org>
2015-11-12 16:54:37 +0100

    camelCase

---
+++ wowza.io.php/example.php
@@ -0,0 +1,24 @@
+<?php
+
+//    site: www.wowza.io
+//    author: Carlos Camacho
+//    email: carloscamachoucv@gmail.com
+//    created: 12/11/2015
+require('wowza.php');
+
+
+
+
+
+$wow = new Wowza("127.0.0.1:8087");
+
+
+//print ($wow->getApplications());
+//print ($wow->createApplication("wakawaka"));
+//print ($wow->deleteApplication("wakawaka"));
+//$wow->dumpApplicationConfig("mocoloco");
+
+
+
+?>
+
--- wowza.io.php/wowza.php
+++ wowza.io.php/wowza.php
@@ -3,27 +3,27 @@
 //    site: www.wowza.io
 //    author: Carlos Camacho
 //    email: carloscamachoucv@gmail.com
-//    created: 12/11/2015
-
+//    created: November 2015
+//    Use camelCase for variable names and method names....
 
 class Wowza{
 
-	private $wowza_server;
+	private $wowzaServer;
 
-	function __construct($wowza_server = "127.0.0.1:8087") {
-		$this->wowza_server = $wowza_server;
+	function __construct($wowzaServer = "127.0.0.1:8087") {
+		$this->wowzaServer = $wowzaServer;
 	}
 
-	function get_wowza_server(){
-		return $this->wowza_server;
+	function getWowzaServer(){
+		return $this->wowzaServer;
 	}
 
-	function set_wowza_server($wowza_server = "127.0.0.1:8087"){
-		$this->wowza_server = $wowza_server;
+	function setWowzaServer($wowzaServer = "127.0.0.1:8087"){
+		$this->wowzaServer = $wowzaServer;
 	}
 
-	function get_applications(){
-		$service_url = "http://localhost:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications";
+	function getApplications(){
+		$service_url = "http://".$this->wowzaServer."/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications";
 		$curl = curl_init($service_url);
 
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -49,9 +49,8 @@ class Wowza{
 		return var_export($decoded->applications);
 	}
 
-
-	function get_application($application_name){
-		$service_url = 'http://localhost:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/'.$application_name;
+	function getApplication($applicationName){
+		$service_url = "http://".$this->wowzaServer."/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/".$applicationName;
 		$curl = curl_init($service_url);
 
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -80,8 +79,39 @@ class Wowza{
 
 	}
 
-	function dump_application_config($application_name){
-		$service_url = 'http://localhost:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/'.$application_name;
+	function deleteApplication($applicationName){
+		$service_url = "http://".$this->wowzaServer."/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/".$applicationName;
+		$curl = curl_init($service_url);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+			'Accept:application/json',
+			'charset=utf-8'
+		));
+		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+		$curl_response = curl_exec($curl);
+
+		if ($curl_response === false) {
+			$info = curl_getinfo($curl);
+			curl_close($curl);
+			die('error occured during curl exec. Additioanl info: ' . var_export($info));
+		}
+
+		curl_close($curl);
+		$decoded = json_decode($curl_response);
+
+		if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+			die('error occured: ' . $decoded->response->errormessage);
+		}
+		//return var_export($decoded);
+		//file_put_contents("asdf.txt", json_encode($decoded, JSON_PRETTY_PRINT));
+		return var_export($decoded, JSON_PRETTY_PRINT);
+
+	}
+
+	function dumpApplicationConfig($applicationName){
+		$service_url = "http://".$this->wowzaServer."/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/".$applicationName;
 		$curl = curl_init($service_url);
 
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -105,20 +135,19 @@ class Wowza{
 			die('error occured: ' . $decoded->response->errormessage);
 		}
 		//return var_export($decoded);
-		file_put_contents($application_name.".json", json_encode($decoded, JSON_PRETTY_PRINT));
+		file_put_contents($applicationName.".json", json_encode($decoded, JSON_PRETTY_PRINT));
 		return true;
 
 	}
 
-
-	function create_application($application_name){
+	function createApplication($applicationName){
 		require './libs/Mustache/Autoloader.php';
 		Mustache_Autoloader::register();
 
 		$m = new Mustache_Engine;
-		$data = array(application_name => $application_name);
+		$data = array(applicationName => $applicationName);
 
-		$service_url = 'http://localhost:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/'.$application_name;
+		$service_url = "http://".$this->wowzaServer."/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/".$applicationName;
 		$curl = curl_init($service_url);
 
 		$config_file = $fichero = file_get_contents('../wowza.io.templates/default_application.json', FILE_USE_INCLUDE_PATH);
@@ -127,7 +156,7 @@ class Wowza{
 		print ($m->render($config_file,$data));
 	
 
-		$curl_post_data = str_replace("###application_name###", $application_name, $config_file);
+		$curl_post_data = str_replace("###applicationName###", $applicationName, $config_file);
 
 
 		//print ($curl_post_data);
@@ -157,24 +186,11 @@ class Wowza{
 		}
 		return var_export($decoded);
 	}
+
+	//To add:
+	// SecureTokens
+	// IP restrictions
 }
-
-
-$wow = new Wowza("127.0.0.1");
-
-
-//print ($wow->get_applications());
-
-
-
-
-print ($wow->create_application("asdf"));
-
-
-//$wow->get_applications();
-//$wow->dump_application_config("mocoloco");
-
-
 
 ?>
 
